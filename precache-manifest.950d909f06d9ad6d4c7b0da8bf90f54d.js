aceeb665ecaddf85621b30304fcbe85fe122058b
pdembol <debicki.piotr90@gmail.com>
2020-10-19 20:54:30 +0200

    Updates

---
+++ precache-manifest.950d909f06d9ad6d4c7b0da8bf90f54d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcb5ee95aab2f20ab0b959bb3b19695",
+    "url": "/test_management/index.html"
+  },
+  {
+    "revision": "a39edf98914f074ba2d7",
+    "url": "/test_management/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "0828d532299048233063",
+    "url": "/test_management/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a39edf98914f074ba2d7",
+    "url": "/test_management/static/js/2.875b8944.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/test_management/static/js/2.875b8944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0828d532299048233063",
+    "url": "/test_management/static/js/main.98354d5e.chunk.js"
+  },
+  {
+    "revision": "e9d39925ae89ba01f6a1",
+    "url": "/test_management/static/js/runtime-main.a725cbe8.js"
+  }
+]);
